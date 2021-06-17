250e7bd0e2e3c0bbb965fb1b7ecaded9e57179a6
yacchan <m33love.ya.ko@gmail.com>
2021-06-17 23:49:56 +0900

    Update stt_IBM.py

--- stt_IBM.py
+++ stt_IBM.py
@@ -15,13 +15,17 @@ speech_to_text.set_service_url(url)
 cont_type = "audio/mp3"
 lang = "ja-JP_BroadbandModel"
 
-# watson connection
-input_file = input('audio file path: ')
-with open(input_file, 'rb') as audio_file:
-    result_json = speech_to_text.recognize(audio=audio_file, content_type=cont_type, model=lang)
+def input_file():
+    # watson connection
+    input_file = input('audio file path: ')
+    with open(input_file, 'rb') as audio_file:
+        result_json = speech_to_text.recognize(audio=audio_file, content_type=cont_type, model=lang)
 
-# print
-output_file = input('output file path: ')
-with open(output_file, 'w') as f:
-    for i in range(len(result_json.result["results"])):
-        f.write(result_json.result["results"][i]["alternatives"][0]["transcript"].replace(' ', '') + '。')
+    # print
+    output_file = input('output file path: ')
+    with open(output_file, 'w') as f:
+        for i in range(len(result_json.result["results"])):
+            f.write(result_json.result["results"][i]["alternatives"][0]["transcript"].replace(' ', '') + '。')
+
+if __name__ == '__main__':
+    input_file()
